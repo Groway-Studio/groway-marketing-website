c6e93b209d6459ca8cb7a08b337747c8976ca9a5
cristiandelcidd <cristian_delcid10@yahoo.com>
2022-06-13 18:16:37 -0600

    Footer styles updated

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,30 +3,36 @@ import styles from "./footer.module.scss";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <img src="./src/assets/groway.svg" alt="Groway Studio" />
-      <div>
-        Escríbenos a: <a href="mailto:hola@groway.studio">hola@groway.studio</a>
-        <img src="./src/assets/social-media/instagram.svg" alt="Instagram" />
-        <img src="./src/assets/social-media/linkedin.svg" alt="Linkdein" />
-        <img src="./src/assets/social-media/facebook.svg" alt="Facebook" />
+      <div className={styles.footer__logo}>
+        <img src="./src/assets/groway.svg" alt="Groway Studio" />
       </div>
       <div>
-        Perú
-        <p>
+        Escríbenos a: <a href="mailto:hola@groway.studio">hola@groway.studio</a>
+        <div className={styles.footer__socialMedia}>
+          <img src="./src/assets/social-media/instagram.svg" alt="Instagram" />
+          <img src="./src/assets/social-media/linkedin.svg" alt="Linkdein" />
+          <img src="./src/assets/social-media/facebook.svg" alt="Facebook" />
+        </div>
+      </div>
+      <div className={styles.footer__address}>
+        <p className={styles.footer__country_name}>Perú</p>
+        <span>
           AV. El Derby 055 Edificio Cronos, Torre 1 Piso 7 Santiago de Surco
           Lima - Perú
-        </p>
+        </span>
+      </div>
+      <div className={styles.footer__phone_numbers}>
         <div>
-          <div>
-            EE.UU
-            <a href="tel:+17867558193">+1 786 7558193</a>
-          </div>
-          <div>
-            Argentina <a href="tel:+541152175854">+54 11 52175854</a>
-          </div>
-          <div>
-            Colombia <a href="tel:+5715800311">+57 15 800311</a>
-          </div>
+          <p className={styles.footer__country_name}>EE.UU</p>
+          <a href="tel:+17867558193">+1 786 7558193</a>
+        </div>
+        <div>
+          <p className={styles.footer__country_name}>Argentina</p>
+          <a href="tel:+541152175854">+54 11 52175854</a>
+        </div>
+        <div>
+          <p className={styles.footer__country_name}>Colombia</p>
+          <a href="tel:+5715800311">+57 15 800311</a>
         </div>
       </div>
     </footer>
