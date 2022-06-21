d51ad78b0c65095087fbcc3c51f22bb49e343eb4
cristiandelcidd <cristian_delcid10@yahoo.com>
2022-06-20 22:18:44 -0600

    Navbar responsive updated

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,9 +1,24 @@
+import { useEffect, useLayoutEffect, useState } from "react";
 import styles from "./navbar.module.scss";
 
 const Navbar = () => {
+  const [show, setShow] = useState(false);
+  const [width, setWidth] = useState(null);
+
+  useEffect(() => {
+    window.addEventListener("resize", () => setWidth(window.innerWidth));
+
+    return () => window.removeEventListener("resize", () => {});
+  }, []);
+
   return (
     <nav className={styles.navbar}>
-      <input type="checkbox" name="checkbox" id="checkbox" />
+      <input
+        type="checkbox"
+        name="checkbox"
+        id="checkbox"
+        onClick={() => setShow(!show)}
+      />
       <label htmlFor="checkbox" className={styles.navbar__burger_menu}>
         <div className={styles.navbar__burger_line}></div>
         <div className={styles.navbar__burger_line}></div>
@@ -17,7 +32,12 @@ const Navbar = () => {
           draggable={false}
         />
       </div>
-      <ul className={styles.navbar__options}>
+      <ul
+        className={`${styles.navbar__options} ${
+          show && width < 600 ? styles.navbar__show : ""
+        }`}
+        onClick={() => setShow(!show)}
+      >
         <li className={styles.navbar__options_item}>
           <a href="#">Inicio</a>
         </li>
